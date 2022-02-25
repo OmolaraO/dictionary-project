e779a86af5729b43a95c1cdfb5d4a94e8c041c32
OmolaraO <ollylarie@gmail.com>
2022-02-25 15:56:01 +0100

    Created the Search Engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
 import './App.css';
-
+import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <Dictionary />
       <header className="App-header">
-        Hello
       </header>
+      <footer>
+        Coded by Omolara
+      </footer>
+      </div>
     </div>
   );
 }
